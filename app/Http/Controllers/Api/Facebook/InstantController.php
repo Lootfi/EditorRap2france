7692a6e876cf7e936c490aa34ac262a4abe4452c
Karam Henni <karam.tenes@gmail.com>
2020-07-11 13:06:56 +0100

    Minor Improvements
    Add App url prefix to the file_get_contents function

--- app/Http/Controllers/Api/Facebook/InstantController.php
+++ app/Http/Controllers/Api/Facebook/InstantController.php
@@ -22,11 +22,10 @@ class InstantController extends Controller
 			);
 
 	$transformer = new Transformer();
-
 	$rules = file_get_contents(__DIR__ .'/simple_rules.json',true);
 	$transformer->loadRules($rules);
     libxml_use_internal_errors(false);
-	$html=file_get_contents('/api/'.$tag.'/getView',true);
+	$html=file_get_contents(url('/'). '/api/'.$tag.'/getView',true);
 	$document = new \DOMDocument();
 	$document->loadHTML($html);
 	libxml_use_internal_errors(true);
