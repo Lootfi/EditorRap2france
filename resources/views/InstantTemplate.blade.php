29475bce7602ada4c80efe186bd682dea7e183b3
Karam Henni <karam.tenes@gmail.com>
2020-07-26 11:06:50 +0100

    Enhance instant articles template

--- resources/views/InstantTemplate.blade.php
+++ resources/views/InstantTemplate.blade.php
@@ -16,7 +16,7 @@
       <time class="op-published" datetime="{{$article->created_at}}">{{$article->created_at}}</time>
       <time class="op-modified" datetime="{{$article->updated_at}}">{{$article->updated_at}}</time>
       <address>
-          <a rel="facebook" href="http://facebook.com/brandon.diamond">{{$article->Creator->Full_Name}}</a>
+          <a rel="facebook" href="http://facebook.com/Rap2FranceOfficiel/">{{$article->Creator->Full_Name}}</a>
         </address>
     </header>
       <!-- Article body goes here -->
@@ -34,29 +34,67 @@
           @if($block['type'] == "paragraph")
             <p>{{$block['data']['text']}}</p>
           @endif
+          @if($block['type'] == "header")
+          <h{{$block['data']['level']}} class="block">{{strip_tags($block['data']['text'])}}</h{{$block['data']['level']}}>
+
+    @endif
+    @if($block['type'] == "list")
+
+      @if($block['data']['style'] == "ordered")
+      <ol >
+        @foreach($block['data']['items'] as $item)
+          <li>{{strip_tags($item)}}</li>
+        @endforeach
+      </ol>
+        
+      @else
+      <ul >
+        @foreach($block['data']['items'] as $item)
+          <li>{{strip_tags($item)}}</li>
+        @endforeach
+      </ul>
+      @endif
+
+    @endif
           @if($block['type'] == "quote")
-            <blockquote>{{$block['data']['text']}}</blockquote>
+            <aside>{{$block['data']['text']}}
+            <cite>{{strip_tags($block['data']['caption'])}}</cite>
+          </aside>
           @endif
           @if($block['type'] == "image")
           @if($block['data']['file'])
-
-              <img src="{{$block['data']['file']['url']}}" />
+            <figure>
+              <img src="{{$block['data']['file']['url']}}" data-fb-disable-360 />
+              <figcaption>{{strip_tags($block['data']['caption'])}}</figcaption>
+            </figure>
           @else
-              <img src="{{$block['data']['url']}}" />
+              <figure>
+              <img src="{{$block['data']['url']}}" data-fb-disable-360 />
+              <figcaption>{{strip_tags($block['data']['caption'])}}</figcaption>
+            </figure>
           @endif
           @endif
           @if($block['type'] == "embed")
             @if($block['data']['service'] == "instagram")
+            <figure class="op-interactive">
           <iframe width="{{$block['data']['width']}}" height="{{$block['data']['height']}}" src="{{$block['data']['embed']}}" frameborder="0"></iframe>
+            <figcaption>{{strip_tags($block['data']['caption'])}}</figcaption>
+        </figure>
 
         @endif
 
         @if($block['data']['service'] == "youtube")
+        <figure class="op-interactive">
           <iframe width="{{$block['data']['width']}}" height="{{$block['data']['height']}}" src="{{$block['data']['embed']}}" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+          <figcaption>{{strip_tags($block['data']['caption'])}}</figcaption>
+        </figure>
 
         @endif
         @if($block['data']['service'] == "twitter")
+        <figure class="op-interactive">
           <iframe width="{{$block['data']['width']}}" height="{{$block['data']['height']}}" src="{{$block['data']['embed']}}"  frameborder="0" border="0"></iframe>
+          <figcaption>{{strip_tags($block['data']['caption'])}}</figcaption>
+        </figure>
         @endif
         @endif
 
