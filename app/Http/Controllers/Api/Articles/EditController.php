f1d708970b1fe91233cf840974a1779e58a1927f
Karam Henni <karam.tenes@gmail.com>
2020-08-06 16:22:14 +0100

    debug

--- app/Http/Controllers/Api/Articles/EditController.php
+++ app/Http/Controllers/Api/Articles/EditController.php
@@ -104,39 +104,7 @@ class EditController extends Controller
             }
 
 
-            foreach(json_decode($article->contenuJSON,true)['blocks'] as $key =>  $block){
-
-                if($block['type'] == 'image' && preg_match('#^https://img.rap2france.com#', $block['data']['file']['url']) != 1 ){
-                        $image = new ImageArticle();
-                        $image->idnews = $article->id;
-                        $image->image = $block['data']['file']['name'];
-                        $image->created_at = now();
-                        $image->updated_at = now();
-                        $image->save(); 
-                        $urltosend = "https://img.rap2france.com/public/medias/r2f_new-downloadimg-corps-dimension.php?imgUrl=".$block['data']['file']['url']."&id=".$image->id."&url=".url('/')."&name=".$image->image;
-
-                        $ch = curl_init($urltosend);
-                       curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-                        curl_setopt($ch, CURLOPT_BINARYTRANSFER, 1);
-                        curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);
-                        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
-                        curl_setopt($ch, CURLOPT_PROXY, "113.52.144.36");
-                        curl_setopt($ch, CURLOPT_PROXYPORT, "9339");
-                        curl_setopt($ch, CURLOPT_PROXYUSERPWD, "allwebnet@gmail.com:dtNj0hSa");
-                        curl_setopt($ch, CURLOPT_HEADER, 0);
-                        curl_setopt($ch, CURLOPT_USERAGENT, 'Mozilla/5.0 (Windows NT 10.0; WOW64; rv:45.0) Gecko/20100101 Firefox/45.0');
-                        $data = curl_exec($ch);
-                        $responseCode = curl_getinfo($ch, CURLINFO_HTTP_CODE);      //Here we fetch the HTTP response code
-
-                        $jsonContent = json_decode($article->contenuJSON,true);
-
-                        $jsonContent['blocks'][$key]['data']['file']['url'] ="https://img.rap2france.com/public/medias/news/image/".$image->id."/raw/mdpi/".$block['data']['file']['name']; 
-
-                        $article->contenuJSON = json_encode($jsonContent);
-                        $article->save();
-                        
-                    }
-                }
+          
 
         
 
