48219b1266c61a4f7c090c10f73723a9e51657ea
Karam Henni <karam.tenes@gmail.com>
2020-08-23 14:24:45 +0100

    Add database syncing api call

--- app/Http/Controllers/Api/Articles/ShowController.php
+++ app/Http/Controllers/Api/Articles/ShowController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Api\Articles;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\Article;
+use DB;
 
 class ShowController extends Controller
 {
@@ -23,5 +24,59 @@ class ShowController extends Controller
     	}
 
     	return response()->json("Article not found");
+    }
+
+
+    public function sync(){
+
+        if(env('R2F_API_KEY') === request()->get('api_key')){
+
+            if(request()->get('action') == "sync"){
+
+               $article =  DB::connection('R2F')
+               ->table('R2F_actualite')
+                          ->select('alaune', 'auteur', 'contenu', 'contenutext', 'created_at', 'dateactu', 'diapo', 'id', 'idcat', 'identifier', 'image', 'keywords', 'program', 'signature', 'tag', 'titre', 'updated_at', 'url')
+                          ->where('id',request()->get('id'))
+                          ->get();
+
+                         
+                if(count($article) != 0 ){
+                    $article = $article[0];
+                     DB::table('r2f_new_actualite_testing_copy')
+                    ->where('id',request()->get('id'))
+                    ->update(['image' => $article->image,
+                               'titre' => $article->titre,
+                               'contenu' => $article->contenu ,
+                                'contenutext' => $article->contenutext,
+                                'updated_at' => $article->updated_at,
+                                'idcat' => $article->idcat,
+                                'tag' =>$article->tag,
+                                'url'=> $article->url,
+                                'dateactu' => $article->dateactu,
+                                'created_at' => $article->created_at,
+                                'alaune' => $article->alaune
+                            ]);
+
+                    return "success updated";
+
+                    }else{
+
+                    DB::table('r2f_new_actualite_testing_copy')
+                    ->where('id',request()->get('id'))->delete();
+
+                    return "Article Not found so Deleted ";
+
+                    }           
+
+            }else{
+
+            
+
+            return "Action not valide";
+        }
+
+        }
+
+        return "no match";
     }
 }
